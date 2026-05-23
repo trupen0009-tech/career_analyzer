63ced3c7f7646a0b8272aa25c174758b36ee322d
trupen0009-tech <trupen0009@gmail.com>
2026-05-23 14:36:33 +0530

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+function App() {
+  return (
+    <div>
+      <h1>AI Career Analyzer</h1>
+
+      <p>Frontend is working 😄🔥</p>
+    </div>
+  );
+}
+
+createRoot(document.getElementById("root")).render(<App />);

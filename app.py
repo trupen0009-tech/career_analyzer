f7f2200f74561da21b84cc9358789ec2df7cb1c4
trupen0009-tech <trupen0009@gmail.com>
2026-05-23 17:14:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,13 +27,13 @@ def call_ai(message):
 
     response = requests.post(API_URL, headers=headers, json=data)
     result = response.json()
-    
-    answer = result["choices"][0]["message"]["content"]
+
     if "choices" not in result:
     print("OpenRouter error response:", result, flush=True)
     return "AI model is temporarily unavailable. Please try again in a few seconds."
-    return answer
 
+    answer = result["choices"][0]["message"]["content"]
+    return answer
 
 def save_to_csv(question, answer):
     print("SAVE FUNCTION STARTED")
